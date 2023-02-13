b8def72049b5cf507fc410cc6806e61437046d8c
Sergio Lezama <sergiodavidlezama@gmail.com>
2023-02-13 02:18:59 +0000

    Cap 07 vimos variables

---
+++ 01_print.py
@@ -0,0 +1,14 @@
+print('Hola mundo')
+
+print('Hola soy Sergio')
+
+print(5 * 5)
+
+print(1 + 2)
+
+#Esto es un comentario, python ignorara esta linea
+'''
+Aqui puedo comentar 
+Esto que son varias lineas
+Python ignorara todo esto
+'''
---
+++ 02_vars.py
@@ -0,0 +1,19 @@
+print('Variables')
+
+my_name = 'Sergio'
+print(my_name)
+
+my_age = 20
+print(my_age)
+
+print('Hola! soy ', my_name, ', tengo ', str(my_age))
+
+my_name = 'David'
+print('Hola! soy ', my_name, ', tengo ', str(my_age))
+
+user_name = input('Cual es tu nombre: ')
+print(user_name)
+
+user_age = input('Cual es tu edad: ')
+print(user_age)
+print('Hola! eres ', user_name, ', tienes ', user_age)
